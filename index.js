7b62bc5b74e68e88016f1ce99e9bc31d71dfa79a
shaune <theshaune@users.noreply.github.com>
2018-07-13 23:35:20 +0700

    away

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {
 } from 'https://unpkg.com/lit-html/lib/lit-extended.js?module'
 
 const Link = html`
-  <a title='View Github' href='https://github.com/theshaune/shaunewest.com'>🕺</a>
+  <a title='View Github' href='https://github.com/theshaune/shaunewest.com'>✈️</a>
 `
 
 const App = () => html`
