98dd1c13f5f62f91158aa4ba304cd5bbd66009e8
shaune <contact@shaunewest.com>
2020-06-10 12:07:32 +1000

    Add thai, add stackoverflow to index

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 import theme from './theme'
 import GlobalStyles from './Components/GlobalStyles/GlobalStyles'
 import GithubIcon from './Components/Icons/Github'
+import StackOverflowIcon from './Components/Icons/StackOverflow'
 
 function App(): JSX.Element {
   return (
@@ -11,15 +12,35 @@ function App(): JSX.Element {
       <GlobalStyles />
       <div className="App">
         <header className="App-header">
-          <p sx={{ fontSize: 3 }}>Hello</p>
-          <a
-            sx={{ color: 'text', width: '2rem' }}
-            href="https://github.com/theshaune"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p sx={{ fontSize: 4 }}>สวัสดีชาวโลก</p>
+          <div
+            sx={{
+              display: 'grid',
+              gap: '1rem',
+              gridTemplateColumns: 'repeat(2, 1fr)',
+            }}
           >
-            <GithubIcon sx={{ fill: 'white', width: 'icon.2' }} />
-          </a>
+            <a
+              sx={{ display: 'inline-block', color: 'text' }}
+              href="https://github.com/theshaune"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubIcon
+                sx={{ display: 'block', fill: 'white', width: 'icon.2' }}
+              />
+            </a>
+            <a
+              sx={{ display: 'inline-block', color: 'text' }}
+              href="https://stackoverflow.com/users/6712681/shaune"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <StackOverflowIcon
+                sx={{ display: 'block', fill: 'white', width: 'icon.2' }}
+              />
+            </a>
+          </div>
         </header>
       </div>
     </ThemeProvider>
